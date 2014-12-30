9e1bd0f26327df59e0d9fcead38d459b1c185c1c
Daniel Kamil Kozar <dkk089@gmail.com>
2014-12-30 01:41:57 +0100

    provide two implementations

--- sadd.c
+++ sadd.c
@@ -1,20 +1,37 @@
 #include <limits.h>
 #include <assert.h>
+#include <stddef.h>
 
 int sadd(int a, int b);
+int sadd_pushf_cmov(int a, int b);
+
+typedef int (*sadd_impl_fn)(int, int);
+
+static const sadd_impl_fn sadd_impls[] = { sadd, sadd_pushf_cmov };
+
+#define SIZE(x) (sizeof(x)/sizeof(*x))
+
+static void test(sadd_impl_fn impl)
+{
+    assert(impl(2,5) == (2+5));
+    assert(impl(-2,-5) == (-2 + -5));
+    assert(impl(0,0) == (0 + 0));
+    assert(impl(-2,5) == (-2 + 5));
+    assert(impl(5,-2) == (5 + -2));
+    assert(impl(INT_MAX, 0) == (INT_MAX + 0));
+    assert(impl(INT_MAX, -5) == (INT_MAX + -5));
+    assert(impl(INT_MAX, 5) == INT_MAX);
+    assert(impl(INT_MIN, 0) == (INT_MIN + 0));
+    assert(impl(INT_MIN, -5) == INT_MIN);
+    assert(impl(INT_MIN, 5) == (INT_MIN + 5));
+}
 
 int main(void)
 {
-    assert(sadd(2,5) == (2+5));
-    assert(sadd(-2,-5) == (-2 + -5));
-    assert(sadd(0,0) == (0 + 0));
-    assert(sadd(-2,5) == (-2 + 5));
-    assert(sadd(5,-2) == (5 + -2));
-    assert(sadd(INT_MAX, 0) == (INT_MAX + 0));
-    assert(sadd(INT_MAX, -5) == (INT_MAX + -5));
-    assert(sadd(INT_MAX, 5) == INT_MAX);
-    assert(sadd(INT_MIN, 0) == (INT_MIN + 0));
-    assert(sadd(INT_MIN, -5) == INT_MIN);
-    assert(sadd(INT_MIN, 5) == (INT_MIN + 5));
+    size_t i;
+    for(i = 0 ; i < SIZE(sadd_impls) ; ++i)
+    {
+        test(sadd_impls[i]);
+    }
     return 0;
 }
--- sadd_impl.asm
+++ sadd_impl.asm
@@ -1,4 +1,4 @@
-bits 32
+bits 64
 global sadd
 sadd:
     add esi, edi
@@ -17,4 +17,24 @@ sadd:
 
 .max:
     mov eax, 0x7fffffff
+    ret
+
+global sadd_pushf_cmov
+sadd_pushf_cmov:
+    mov ecx, 0x80000000
+    mov edx, 0x7fffffff
+    
+    add esi, edi
+    
+    pushf
+    pop rax
+    not rax
+    
+    test ax, 0x0801
+    cmovz esi, ecx
+    
+    test ax, 0x0880
+    cmovz esi, edx
+
+    mov eax, esi
     ret
